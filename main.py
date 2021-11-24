6f88575826552c39da14566436375605159795de
AirmanZZZ <arm.cuentas@gmail.com>
2021-11-24 13:53:42 +0100

    Añadido main.py que funciona correctamente

--- main.py
+++ main.py
@@ -3,12 +3,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+app = FastAPI()
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/listar")
+def listar():
+    f = open("demofile.txt", "r")
+    return f.read()
+
+@app.post("/alta/{nombre}/{telefono}")
+def dar_alta(nombre: str, telefono: str):
+    f = open("demofile.txt", "w")
+    # {
+    # "nombre": "pepe",
+    # "telefono": "5551234"
+    # }
+    f.write("{\"nombre\":\"" + nombre + "\",\"telefono\":\"" + telefono + "\"}")
+    f.close()
+    return {"alta":"ok"}
 
